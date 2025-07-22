c265ce1f710495a7cfa3d20552fd29940be6809a
Davi Machado Moureira <davi20021@outlook.com>
2025-07-22 07:50:41 -0300

    Criação da arquitetura do projeto!

---
+++ src/main/java/com/darffin/Main.java
@@ -0,0 +1,24 @@
+package com.darffin;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/darffin/MainView.fxml")));
+        Scene scene = new Scene(root);
+        stage.setTitle("Deck Builder");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
