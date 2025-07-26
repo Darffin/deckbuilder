cb67fc397e8f1126382f6959d002f7d97b2861da
Davi Machado Moureira <davi20021@outlook.com>
2025-07-26 08:05:32 -0300

    refactor: improve project architecture and controller/service separation

---
+++ src/main/java/com/darffin/service/GameService.java
@@ -0,0 +1,13 @@
+package com.darffin.service;
+
+import com.darffin.model.Player;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class GameService {
+
+
+    public void damageEnemy(int damage) {
+        //player.setLife(player.getLife() - damage);
+    }
+
+}
