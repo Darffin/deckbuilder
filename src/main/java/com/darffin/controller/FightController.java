91e7ab3d28bd10a8dfd570acd3d504ea41ac494b
Davi Machado Moureira <davi20021@outlook.com>
2025-07-26 08:00:31 -0300

    feat: trigger card effects correctly when buttons are pressed

--- src/main/java/com/darffin/controller/FightController.java
+++ src/main/java/com/darffin/controller/FightController.java
@@ -2,6 +2,11 @@ package com.darffin.controller;
 
 import com.darffin.model.Card;
 import com.darffin.model.Player;
+import com.darffin.service.CardEffectService;
+import com.darffin.service.CardService;
+import com.darffin.service.EnemyService;
+import com.darffin.service.PlayerService;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,9 +17,8 @@ import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Collections;
+import java.lang.reflect.Method;
+import java.util.*;
 
 @Component
 public class FightController {
@@ -43,42 +47,77 @@ public class FightController {
 
     @Autowired
     private PlayerController playerController;
-    Player player = Player.getInstance();
 
-    private List<Card> deckPlayer = new ArrayList<Card>();
+    @Autowired
+    private CardController cardController;
+
+    @Autowired
+    PlayerService playerService;
+
+    @Autowired
+    CardService cardService;
+
+    @Autowired
+    CardEffectService cardEffectService;
+
+    @Autowired
+    EnemyService enemyService;
+
+
 
     public FightController(){
     }
 
     public void prepareNewGame(){
-        playerLife.setText(player.getLife()+"");
-        playerController.defaultDeck();
-        this.deckPlayer = player.getDeck();
+        playerLife.setText(playerService.playerLife()+"");
+        enemyName.setText(enemyService.enemyName());
+        enemyLife.setText(enemyService.enemyLife()+"");
+        playerService.setPlayerDeckDefault();
 
-        Collections.shuffle(deckPlayer);
+        cardController.cardButtonsUpdate(card1,card2,card3,card4);
 
-        card1.setText(deckPlayer.get(0).getName());
-        deckPlayer.remove(0);
-        card2.setText(deckPlayer.get(1).getName());
-        deckPlayer.remove(1);
-        card3.setText(deckPlayer.get(2).getName());
-        deckPlayer.remove(2);
-        card4.setText(deckPlayer.get(3).getName());
-        deckPlayer.remove(3);
-
-        deck.setText(deckPlayer.size()+"");
+        updateDecksLabel();
 
     }
 
-    public void cardAction(){
+    public void cardAction(ActionEvent event){
+        Button clickedButton = (Button) event.getSource();
+        Card selectedCard = cardController.getCardByName(clickedButton.getText());
+        String methodName = selectedCard.getName().trim().replaceAll("\\s+", "");
 
+        try {
+            Method method = CardEffectService.class.getMethod(methodName);
+            method.invoke(cardEffectService);
+            enemyLife.setText(enemyService.enemyLife() + "");
+
+            cardService.addToDiscardDeck(selectedCard);
+
+            //playerService.verifyDeckAvailability();
+
+            clickedButton.setText(playerService.playerDeck().get(0).getName());
+            playerService.playerDeck().remove(0);
+
+            playerService.verifyDeckAvailability();
+
+            updateDecksLabel();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        //Preciso pegar uma carta do deck e atualizar o botão
+    }
+
+    public void updateDecksLabel(){
+        deck.setText(playerService.playerDeck().size()+"");
+        discardDeck.setText(cardService.getDiscardDeck().size()+"");
     }
 
     public void playerTurn(){
-        card1.setText(deckPlayer.get(0).getEffect());
-        card2.setText(deckPlayer.get(1).getEffect());
-        card3.setText(deckPlayer.get(2).getEffect());
-        card4.setText(deckPlayer.get(3).getEffect());
+        card1.setText(playerService.playerDeck().get(0).getEffect());
+        card2.setText(playerService.playerDeck().get(1).getEffect());
+        card3.setText(playerService.playerDeck().get(2).getEffect());
+        card4.setText(playerService.playerDeck().get(3).getEffect());
 
     }
 
